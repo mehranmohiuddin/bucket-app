fb5c05f9885dbcfd9803cc4b22dd2052936281d3
mehranmohiuddin <mohiuddin.mehran@gmail.com>
2024-05-27 22:06:36 -0400

    adding logic to download file from bucket, update, and re-uplaod it

--- main.go
+++ main.go
@@ -1,14 +1,63 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io/ioutil"
+	"log/slog"
+	"time"
 
-	fileUpdate "github.com/mehranmohiuddin/bucket-app/internal/file_update"
+	"cloud.google.com/go/storage"
+	"google.golang.org/api/option"
+)
+
+const (
+	bucketName = ""
+	fileName   = ""
 )
 
 func main() {
-	fmt.Println("Hello from bucket app")
+	slog.Info("Hello from bucket app")
 
-	workflow := fileUpdate.WorkflowImpl{}
-	workflow.Process()
+	ctx := context.Background()
+
+	// Initialize client with service account credentials
+	client, err := storage.NewClient(ctx, option.WithCredentialsFile("service-account.json"))
+	if err != nil {
+		slog.Error("Failed to create client: %v", err)
+		return
+	}
+	defer client.Close()
+
+	// Get the file from the bucket
+	rc, err := client.Bucket(bucketName).Object(fileName).NewReader(ctx)
+	if err != nil {
+		slog.Error("Failed to get object: %v", err)
+		return
+	}
+	defer rc.Close()
+
+	data, err := ioutil.ReadAll(rc)
+	if err != nil {
+		slog.Error("Failed to read object: %v", err)
+		return
+	}
+
+	// Append the current date to the file content
+	currentDate := time.Now().Format(time.RFC3339)
+	updatedData := fmt.Sprintf("%s\nUpdated on: %s", string(data), currentDate)
+
+	// Upload the updated file back to the bucket
+	wc := client.Bucket(bucketName).Object(fileName).NewWriter(ctx)
+	wc.ContentType = "text/plain"
+	if _, err := wc.Write([]byte(updatedData)); err != nil {
+		slog.Error("Failed to write object: %v", err)
+		return
+	}
+	if err := wc.Close(); err != nil {
+		slog.Error("Failed to close writer: %v", err)
+		return
+	}
+
+	slog.Info("File updated successfully")
 }
